a46e0d906a1f2d0d67f3c532f9dadb8d2ee35924
iris112 <nglayi99@gmail.com>
2025-06-02 14:05:23 +0900

    deploy subgraph on subtensor evm

--- config/subtensor/chain.ts
+++ config/subtensor/chain.ts
@@ -2,8 +2,8 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
 export const FACTORY_ADDRESS = '0x20D0Cdf9004bf56BCa52A25C9288AAd0EbB97D59'
 
-export const REFERENCE_TOKEN = '0x9Dc08C6e2BF0F1eeD1E00670f80Df39145529F81'
-export const STABLE_TOKEN_POOL = '0x6647dcbeb030dc8E227D8B1A2Cb6A49F3C887E3c'
+export const REFERENCE_TOKEN = '0x9dc08c6e2bf0f1eed1e00670f80df39145529f81'
+export const STABLE_TOKEN_POOL = '0x6647dcbeb030dc8e227d8b1a2cb6a49f3c887e3c'
 
 export const TVL_MULTIPLIER_THRESHOLD = '2'
 export const MATURE_MARKET = '1000000'
@@ -19,11 +19,11 @@ export const ROLL_DELETE_MINUTE_LIMITER = BigInt.fromI32(1000)
 // usually tokens that many tokens are paired with s
 export const WHITELIST_TOKENS: string[] = [
   REFERENCE_TOKEN, // WETH
-  '0xB833E8137FEDf80de7E908dc6fea43a029142F20', // USDC
+  '0xb833e8137fedf80de7e908dc6fea43a029142f20', // USDC
 ]
 
 export const STABLE_COINS: string[] = [
-  '0xB833E8137FEDf80de7E908dc6fea43a029142F20', // USDC
+  '0xb833e8137fedf80de7e908dc6fea43a029142f20', // USDC
 ]
 
 export const SKIP_POOLS: string[] = []
